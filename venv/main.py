2a2c6d0b04ef4dd84071b51d370b50470f49bb11
Cuevaza <viejoviscaino@gmail.com>
2023-12-28 18:22:03 -0600

    inicio 1

--- venv/main.py
+++ venv/main.py
@@ -23,7 +23,7 @@ start_date = int(datetime.strptime(start_date, "%d %b %Y").timestamp() * 1000)
 end_date = int(datetime.strptime(end_date, "%d %b %Y").timestamp() * 1000)
 
 # Obtiene el historial de operaciones
-trades = client.get_my_trades(symbol=symbol, startTime=start_date, endTime=end_date, limit=2000)
+trades = client.get_my_trades(symbol=symbol, startTime=start_date, endTime=end_date, limit=1000)
 
 # Inicializa las sumas de los valores netos de compra y venta
 total_buy_value = 0
@@ -33,22 +33,39 @@ total_sell_value = 0
 buy_counter = 0
 sell_counter = 0
 
+# Inicializa las listas de operaciones de compra y venta
+buy_trades = []
+sell_trades = []
+
+# Separa las operaciones de compra y venta
+for trade in trades:
+    # Convierte la marca de tiempo a un formato de fecha y hora
+    timestamp = datetime.fromtimestamp(trade['time'] / 1000).strftime("%m-%d-%Y %H:%M:%S")
+    # Formatea el precio como decimal con 7 dígitos de precisión
+    price = "{:.7f}".format(float(trade['price']))
+    if trade['isBuyer']:
+        buy_counter += 1
+        total_buy_value += float(price) * float(trade['qty'])
+        buy_trades.append([trade['id'], price, trade['qty'], float(price) * float(trade['qty']), total_buy_value, buy_counter, timestamp])
+    else:
+        sell_counter += 1
+        total_sell_value += float(price) * float(trade['qty'])
+        sell_trades.append([trade['id'], price, trade['qty'], float(price) * float(trade['qty']), total_sell_value, sell_counter, timestamp])
+
 # Escribe los datos en un archivo CSV
 with open(f'{symbol}_trades.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     # Escribe los encabezados de las columnas
-    writer.writerow(["ID", "Precio Compra", "Cantidad Compra", "Valor Neto Compra", "Total Valor Neto Compra", "Número de Compras", "Fecha y Hora Compra", "Es el Mejor Precio de Compra?", "Precio Venta", "Cantidad Venta", "Valor Neto Venta", "Total Valor Neto Venta", "Número de Ventas", "Fecha y Hora Venta", "Es el Mejor Precio de Venta?"])
+    writer.writerow(["ID Compras", "Precio Compra", "Cantidad Compra", "Valor Neto Compra", "Total Valor Neto Compra", "Número de Compras", "Fecha y Hora Compra", "ID Ventas", "Precio Venta", "Cantidad Venta", "Valor Neto Venta", "Total Valor Neto Venta", "Número de Ventas", "Fecha y Hora Venta"])
     # Escribe los datos de cada operación
-    for trade in trades:
-        # Convierte la marca de tiempo a un formato de fecha y hora
-        timestamp = datetime.fromtimestamp(trade['time'] / 1000).strftime("%m-%d-%Y %H:%M:%S")
-        # Formatea el precio como decimal con 7 dígitos de precisión
-        price = "{:.7f}".format(float(trade['price']))
-        if trade['isBuyer']:
-            buy_counter += 1
-            total_buy_value += float(price) * float(trade['qty'])
-            writer.writerow([trade['id'], price, trade['qty'], float(price) * float(trade['qty']), total_buy_value, buy_counter, timestamp, trade['isBestMatch'], '', '', '', '', '', ''])
+    for i in range(max(len(buy_trades), len(sell_trades))):
+        row = []
+        if i < len(buy_trades):
+            row.extend(buy_trades[i])
         else:
-            sell_counter += 1
-            total_sell_value += float(price) * float(trade['qty'])
-            writer.writerow([trade['id'], '', '', '', '', '', '', price, trade['qty'], float(price) * float(trade['qty']), total_sell_value, sell_counter, timestamp, trade['isBestMatch']])
+            row.extend(['', '', '', '', '', '', ''])
+        if i < len(sell_trades):
+            row.extend(sell_trades[i])
+        else:
+            row.extend(['', '', '', '', '', '', ''])
+        writer.writerow(row)
